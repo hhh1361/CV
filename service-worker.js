94c1ebad0ba70a7aa476c6baa0cf0aae4c667811
hhh1361 <nikita.turlyko@gmail.com>
2019-10-31 14:41:58 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/CV/precache-manifest.880a81d145c4d61f2044316135551c2a.js"
+  "/CV/precache-manifest.9d50aa54bd299be69c71582088476e0b.js"
 );
 
 self.addEventListener('message', (event) => {
